e69564c3791b4050fb8efe1cab664620f6d4b1cb
mayconbalves <mayconbalves1@gmail.com>
2025-10-16 21:45:08 -0300

    feat: created main section in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
 import styles from "./page.module.css";
+import { JSX } from "react";
+import Image from "next/image";
 
-export default function Home() {
-  return <div className={styles.page}></div>;
+export default function Home(): JSX.Element {
+  return (
+    <div className={styles.main}>
+      <section>
+        <div className={styles.slider}>
+          <hr />
+          <div className={styles.container}>
+            <div className={styles.container_title}>
+              <h1>Maycon Alves</h1>
+            </div>
+            <div className={styles.container_ribbon}>
+              <h2>
+                <span>Desenvolvedor Front-End</span>
+              </h2>
+            </div>
+            <div className={styles.container_image}>
+              <Image
+                src="https://picsum.photos/200/300"
+                alt="Maycon Alves"
+                width={500}
+                height={500}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
